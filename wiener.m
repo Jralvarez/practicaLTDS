b4ebc6b8d8de75ddec8287554cb4d5ddfd75a8e1
José Ramón Álvarez Rojo <chuckelbarak@gmail.com>
2012-11-16 06:29:38 +0100

    Hay un error en la implementación LS de la funcion wiener

---
+++ wiener.m
@@ -0,0 +1,51 @@
+%%
+% José Ramón Álvarez Rojo 100038962@alumnos.uc3m.es
+% Noviembre de 2012
+%
+% Devuelve un vector con los P coeficientes del
+% filtro de Wiener tal que x[n] =~ w[n] * p[n]
+%
+% El parámetro impl permite elegir entre una
+% implementación según las ecuaciones de Wiener-Hopfs
+% (impl='wienerhopfs', por defecto) 
+% y una basada en solución mínimos cuadrados
+% (impl='ls' (el resultado debería ser el mismo
+% con cualquiera de las dos implementciones).
+%
+function [w Ryy ryx] = wiener(y, x, P, impl)
+	if(nargin < 4 || impl=='wienerhopfs')
+		% Implementación basada en las ecuaciones
+		% de Wiener-Hopfs
+		M = P - 1;
+		
+		rxy = xcorr(x, y, M, 'unbiased');
+		ryy = xcorr(y, y, M, 'unbiased');
+
+		rxy = rxy(end-M:end);
+		ryy = ryy(end-M:end);
+
+		Ryy = toeplitz(ryy);
+		w = inv(Ryy)*rxy';
+	else
+		% Implementación basada en solución
+		% mínimos cuadrados
+		% 
+		% Suponiendo P = 2
+		%   x[n] = w[0] y[n]   + w[1] y[n-1];
+		% x[n-1] = w[0] y[n-1] + w[1] y[n-12;
+		%
+		%   x[1] = w[0] y[1]   + w[1] y[0];
+		% 
+		% Matricialmente x = Y w => w = ((Y' Y)^-1) Y' x = pinv(Y)*x
+
+		Y = [];
+		for p = 0:P-1
+			Y = [Y y(end-p:1-1:P-p).'];
+		end
+
+		Ryy = Y.'*Y./(length(x)-P);
+		ryx = Y.'*x(end:-1:P).'/(length(x) - P);
+
+		w = inv(Ryy)*ryx;
+	end
+end
